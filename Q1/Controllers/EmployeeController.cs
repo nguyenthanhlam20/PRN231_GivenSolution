8902eb141854535ac45b9a951836237dc4a9178b
nguyenthanhlam20 <@Lam2001@>
2024-03-17 12:00:38 +0700

    ..

--- Q1/Controllers/EmployeeController.cs
+++ Q1/Controllers/EmployeeController.cs
@@ -11,7 +11,7 @@ namespace Q1.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class EmployeeController : ODataController
+    public class EmployeeController : ControllerBase
     {
         private readonly Spring24B1_ScriptContext _dbContext;
         private readonly IMapper _mapper;
@@ -23,9 +23,49 @@ namespace Q1.Controllers
 
         [EnableQuery]
         [HttpDelete("Delete/{EmployeeId}")]
-        public IActionResult GetAllOrder( int EmployeeId)
+        public IActionResult DeleteEmployeeById(int EmployeeId)
         {
-            return Ok();
+            try
+            {
+                Employee? employee = _dbContext.Employees.SingleOrDefault(e => e.EmployeeId == EmployeeId);
+
+                if (employee == null)
+                {
+                    return NotFound($"No employee with id {EmployeeId}");
+                }
+
+                employee.EmployeeSkills = _dbContext.EmployeeSkills.Where(x => x.EmployeeId == EmployeeId).ToList();
+                employee.Departments = (from emp in _dbContext.Employees
+                                        join dept in _dbContext.Departments on emp.DepartmentId equals dept.ManagerId
+                                        where emp.EmployeeId == EmployeeId
+                                        select dept).ToList();
+
+                employee.EmployeeProjects = _dbContext.EmployeeProjects.Where(e => e.EmployeeId == EmployeeId).ToList();
+
+                int numberOfSkills = employee.EmployeeSkills.Count;
+                int numberOfDepartments = employee.Departments.Count;
+                int numberOfProjects = employee.EmployeeProjects.Count;
+
+                _dbContext.RemoveRange(employee.EmployeeSkills);
+                _dbContext.RemoveRange(employee.EmployeeProjects);
+                _dbContext.Remove(employee);
+
+
+                if (_dbContext.SaveChanges() > 0)
+                {
+                    return Ok(new
+                    {
+                        numberOfProjects,
+                        numberOfSkills,
+                        numberOfDepartments
+                    });
+                }
+                return BadRequest("Cannot delete employee");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
     }
