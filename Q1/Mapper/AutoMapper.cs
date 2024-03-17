87d32059ff7b190f6d5cb798d93c593a080c8a77
nguyenthanhlam20 <@Lam2001@>
2024-03-17 10:09:20 +0700

    .

---
+++ Q1/Mapper/AutoMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+//using Q1.DTOs;
+//using Q1.Models;
+
+namespace Q1.Mapper
+{
+    public class AutoMapper : Profile
+    {
+        public AutoMapper()
+        {
+            //CreateMap<Order , OrderDTO>()
+            //    .ForMember(o => o.CustomerName, opt => opt.MapFrom(src => src.Customer!.CompanyName))
+            //            .ForMember(o => o.EmployeeName, opt => opt.MapFrom(o => o.Employee!.FirstName + ' ' + o.Employee!.LastName))
+            //            .ForMember(o => o.EmployeeDepartmentId, opt => opt.MapFrom(o => o.Employee!.DepartmentId))
+            //            .ForMember(o => o.EmployeeDepartmentName, opt => opt.MapFrom(o => o.Employee!.Department!.DepartmentName));
+            //CreateMap<Customer, DTOs.CustomerDTO>();
+            //CreateMap<DTOs.CustomerDTO, Customer>();
+        }
+    }
+}
---
+++ Q1/Program.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.OData;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+//using Q1.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddControllers().AddOData(options =>
+{
+    options.Select().Filter().Count().OrderBy().Expand().SetMaxTop(100);
+    // Bật tính năng EnableQuery
+    options.EnableQueryFeatures();
+
+});
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+/*builder.Services.AddAutoMapper(typeof(Program).Assembly);*/
+//builder.Services.AddDbContext<PRN_Sum22_B1Context>();
+builder.Services.AddCors(builder =>
+    builder.AddPolicy("corsapp", b => { b.WithOrigins("*").AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin(); }));
+
+
+//MAPPER
+var mapperConfig = new MapperConfiguration(mc => { mc.AddProfile(new Q1.Mapper.AutoMapper()); });
+var mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("corsapp");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
